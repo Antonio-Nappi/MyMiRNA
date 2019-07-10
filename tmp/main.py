c1e46abecff5d1c22ad571bd5f3edcd7f319dbf7
Vicodin996 <a.nappi32@studenti.unisa.it>
2019-07-10 13:08:32 +0200

    Seconda Funzione

--- tmp/main.py
+++ tmp/main.py
@@ -9,17 +9,17 @@ def fastqc(filename):
     if error is not None:
         raise Exception("FASTQC error!")
 
-
-adapter = "TGGAATTCTCGGGTGCCAAGG"
-in_file = "Norm_1_reduced.fastq"
-out_file = "Norm_1_reduced_trimmed.fastq"
-
-command = "cutadapt -a {0} -o {1} {2} -j {3} -q {4} --discard-untrimmed -M {5} -m {6}".format(adapter,
+def cutadapt(filename, adapter="TGGAATTCTCGGGTGCCAAGG"):
+    in_file = filename
+    out_file = "Norm_1_reduced_trimmed.fastq"
+    command = "cutadapt -a {0} -o {1} {2} -j {3} -q {4} --discard-untrimmed -M {5} -m {6}".format(adapter,
                                                                                        out_file,
                                                                                        in_file,
                                                                                        8, 20, 35, 10)
-p = subprocess.Popen(shlex.split(command))
-out, err = p.communicate()
-print(out, err)
-fastqc(out_file)
+    p = subprocess.Popen(shlex.split(command))
+    out, err = p.communicate()
+    print(out, err)
+    fastqc(out_file)
 
+def bowtie():
+reference = ""
