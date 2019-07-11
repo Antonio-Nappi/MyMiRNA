0de3dd7cceccb1c67083869ee3dd2c72f30de300
TestaDiRapa <v.c.pierro@gmail.com>
2019-07-11 17:51:45 +0200

    Added featureCounts function

--- my_mirna.py
+++ my_mirna.py
@@ -1,20 +1,21 @@
 from utils import run_command
 
-'''
- The method checks the quality of the reads in the pre-processing step.
- The tool used to compute the QC is FastQC.
-'''
+
 def fastqc(filename):
+    '''
+     The method checks the quality of the reads in the pre-processing step.
+     The tool used to compute the QC is FastQC.
+    '''
     fastqc_command = "fastqc {}".format(filename)
     return run_command(fastqc_command)
 
-'''
- In the pre-processing step it is necessary to trim the adapters.
- The tool used is CutAdapt
-'''
-def cutadapt(filename, adapter="TGGAATTCTCGGGTGCCAAGG"):
+
+def cutadapt(filename, out_file = "Norm_1_reduced_trimmed.fastq", adapter="TGGAATTCTCGGGTGCCAAGG"):
+    '''
+     In the pre-processing step it is necessary to trim the adapters.
+     The tool used is CutAdapt
+    '''
     in_file = filename
-    out_file = "Norm_1_reduced_trimmed.fastq"
     command = "cutadapt -a {0} -o {1} {2} -j {3} -q {4} --discard-untrimmed -M {5} -m {6}".format(adapter,
                                                                                        out_file,
                                                                                        in_file,
@@ -23,34 +24,37 @@ def cutadapt(filename, adapter="TGGAATTCTCGGGTGCCAAGG"):
     print(out_file)
     fastqc(out_file)
 
-'''
- This function allows to index the files to speed up the mapping process.
- The tool used is Bowtie
-'''
+
 def indexing(filename):
+    '''
+     This function allows to index the files to speed up the mapping process.
+     The tool used is Bowtie
+    '''
     in_file = filename
     out_file = filename+"indexed"
     command = "bowtie-build -f {0} {1}".format(in_file,out_file)
     return run_command(command)
 
-'''
- This function converts a bam file into a fastq file
-'''
+
 def bam_to_fastq(filename):
+    '''
+     This function converts a bam file into a fastq file
+    '''
     in_file = filename
     out_file = in_file[:len(in_file)-3]+"fastq"
     command = "samtools fastq {0} > {1}".format(in_file,out_file)
     return run_command(command)
 
-'''
- This function allows to discover unique reads against the reference genome
-'''
-def mapping_butter(in_file, ref_genome, n_core):
-    command = "butter --mismatches {0} --aln_cores {1} --max_rep {2} {3} {4}".format(1, n_core, 5000, in_file, ref_genome)
+
+def mapping_shortstack(in_file, ref_genome, n_core):
+    '''
+     This function allows to discover unique reads against the reference genome
+    '''
+    command = "ShortStack --nohp --readfile {0} --genome {1}".format(in_file, ref_genome)
     return run_command(command)
 
-def mapping_tophat(in_file,mode,cores):
-    #bisogna indicizzare il file hairpin e mature
-    #il comando cambia a seconda della modalità
-    command = "tophat --bowtie1 -N 1 --library-type=fr-unstrand -p {0} /home/antonio/Scrivania/idexed/mature indicizzato {1}".format(cores,in_file)
+
+def feature_counts(in_file):
+    out_file = "out"
+    command = "featureCounts -a hsa.gtf -o {0} {1}".format(out_file, in_file)
     return run_command(command)
