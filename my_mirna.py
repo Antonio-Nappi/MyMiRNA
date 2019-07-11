78b71d46a1419739520a0749b525ed6a6cbb4586
alex-mont <44395427+alex-mont@users.noreply.github.com>
2019-07-11 12:22:26 +0200

    mapping with butter

--- my_mirna.py
+++ my_mirna.py
@@ -1,11 +1,17 @@
 from utils import run_command
 
-
+'''
+ The method checks the quality of the reads in the pre-processing step.
+ The tool used to compute the QC is FastQC.
+'''
 def fastqc(filename):
     fastqc_command = "fastqc {}".format(filename)
     return run_command(fastqc_command)
 
-
+'''
+ In the pre-processing step it is necessary to trim the adapters.
+ The tool used is CutAdapt
+'''
 def cutadapt(filename, adapter="TGGAATTCTCGGGTGCCAAGG"):
     in_file = filename
     out_file = "Norm_1_reduced_trimmed.fastq"
@@ -16,12 +22,19 @@ def cutadapt(filename, adapter="TGGAATTCTCGGGTGCCAAGG"):
     out_file = run_command(command)
     fastqc(out_file)
 
-
+'''
+ This function allows to index the files to speed up the mapping process.
+ The tool used is Bowtie
+'''
 def indexing(filename):
     in_file = filename
     out_file = filename+"indexed"
     command = "bowtie-build -f {0} {1}".format(in_file,out_file)
     return run_command(command)
 
-
-
+'''
+ This function allows to discover unique reads against the reference genome
+'''
+def mapping_butter(in_file, ref_genome, n_core):
+    command = "butter --mismatches {0} --aln_cores {1} --max_rep {2} {3} {4}".format(1, n_core, 5000, in_file, ref_genome)
+    return run_command(command)
