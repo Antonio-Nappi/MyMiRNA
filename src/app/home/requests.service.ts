6d51243ca0549db96ec6cbb5761a9de9d19512dc
alex-mont <44395427+alex-mont@users.noreply.github.com>
2019-07-19 12:12:40 +0200

    update

--- src/app/home/requests.service.ts
+++ src/app/home/requests.service.ts
@@ -1,13 +1,18 @@
 import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+export interface Res {
+    mature: string[];
+    pre: string[];
+}
+
 @Injectable({
     providedIn: 'root'
 })
 
 export class RequestService {
 
-    constructor(private http: HttpClient) {}
+    constructor(private http: HttpClient) { }
 
     trimmingStep(body: string) {
         const path = 'http://localhost:8080/trimming';
@@ -17,7 +22,8 @@ export class RequestService {
             {
                 headers: new HttpHeaders({
                     'Content-Type': 'application/json',
-                })
+                }),
+                responseType: 'text'
             });
     }
 
@@ -29,11 +35,12 @@ export class RequestService {
             {
                 headers: new HttpHeaders({
                     'Content-Type': 'application/json',
-                })
+                }),
+                responseType: 'text'
             });
     }
 
-    mirnaDetection(body: string) {
+    mirnaAnalysis(body: string) {
         const path = 'http://localhost:8080/mirna';
         return this.http.post(
             path,
@@ -41,7 +48,21 @@ export class RequestService {
             {
                 headers: new HttpHeaders({
                     'Content-Type': 'application/json',
-                })
+                }),
+                responseType: 'text'
+            });
+    }
+
+    mirnaDetection() {
+        const path = 'http://localhost:8080/mirnas';
+        return this.http.get<Res>(path);
+    }
+
+    mirnaInformation(path: string) {
+        return this.http.get(
+            path,
+            {
+                responseType: 'text'
             });
     }
 }
